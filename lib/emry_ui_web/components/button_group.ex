2296fb4f028923f3cac8a558d5fef46697324eea
Thomas Taylor <thmsmtylr@gmail.com>
2022-08-01 19:47:16 +1000

    Setup button group

---
+++ lib/emry_ui_web/components/button_group.ex
@@ -0,0 +1,14 @@
+defmodule EmryUiWeb.Components.ButtonGroup do
+  @moduledoc """
+  Button groups combine sets of buttons into toolbars or split buttons for
+  more complex components. Button groups are also useful for acting as mini
+  “tabs” in UI, for example, switching between date ranges.
+  """
+  use Surface.Component
+
+  def render(assigns) do
+    ~F"""
+    <div></div>
+    """
+  end
+end
---
+++ test/emry_ui_web/components/button_group_test.exs
@@ -0,0 +1,8 @@
+defmodule EmryUiWeb.Components.ButtonGroupTest do
+  use EmryUiWeb.ConnCase, async: true
+  use Surface.LiveViewTest
+
+  alias EmryUiWeb.Components.ButtonGroup
+
+  @endpoint Endpoint
+end
